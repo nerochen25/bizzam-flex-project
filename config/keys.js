12d5e7652416436d5bf5729bf690837ae863bf70
Nero <nerochen25@berkeley.edu>
2018-12-29 15:30:04 -0800

    Modify gitignore

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://bizzam:password123@ds245234.mlab.com:45234/bizzam-database',
+    secretOrKey: "secret"
+              //Make sure this is your own unique string
+  }
