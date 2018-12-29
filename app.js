aa27bd98edfbe7159e3377e738d84f4d96dad66b
Nero <nerochen25@berkeley.edu>
2018-12-28 20:05:12 -0800

    Add test, current, login, register routes

--- app.js
+++ app.js
@@ -3,21 +3,35 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
+const User = require('./models/User');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+  }
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+    
+    app.use(passport.initialize());
+    require('./config/passport')(passport);
 
-  app.use("/api/users", users);
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello Dante V, and Nero"));
+
+app.use("/api/users", users);
 
 
 const port = process.env.PORT || 5000;
 
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
---
+++ validation/login.js
@@ -0,0 +1,27 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateLoginInput(data) {
+    let errors = {};
+
+    data.username = validText(data.username) ? data.username : "";
+    data.password = validText(data.password) ? data.password : "";
+
+    //Validator is the library
+    // if (!Validator.isusername(data.username)) {
+    //     errors.username = 'username is invalid';
+    // }
+
+    if (Validator.isEmpty(data.username)) {
+        errors.username = 'username field is required';
+    }
+
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "Password field is required";
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    };
+};
