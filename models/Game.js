3f05cb0a56f0e466b14bbd08c7f92bc496e4f91b
trevor-shepard <trevorbshepard@gmail.com>
2018-12-30 10:48:28 -0800

    Refactor game model to have both game and board model:

--- models/Game.js
+++ models/Game.js
@@ -2,26 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-// const root = 'https://s3.amazonaws.com/ourbank>';
-const subSquares = {
-    text: String, // Generated from squares on board creation
-    checked: { type: Boolean, default: false },
-    position: Number,
-    // getter attribute for uploading/storing proof for adventure mode
-    // proof: {
-    //     type: String,
-    //     get: v => `${root}${v}`
-    //   }
-}
-
-const subBoards = {
-    userID: Number,
-    squares: [ subSquares ]
-}
 
 const gameSchema = Schema({
-    boards: [ subBoards ],
-    themeID: { type: Schema.Types.ObjectId, ref: 'Theme' }, // belongs to Game
+    boards: [{ type: Schema.Types.ObjectId, ref: 'Game' }],
+    theme: { type: Schema.Types.ObjectId, ref: 'Theme' }, // belongs to Theme
     winner_id: Number,
     gameType: String 
 
