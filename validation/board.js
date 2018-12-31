cbf40e9a6dad9990e4d2e9248fba66922ed882c3
Cjewett_work <Craig9907@gmail.com>
2018-12-30 19:20:08 -0800

    Update board validation with more comments

---
+++ validation/board.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+// So board should validate the user
+// userID: Number exists
+
+// that they are not already playing in this game (already have a board)
+// userID: Number < search the game if the userId is included already >
+
+
+// validate that the game exists
+
+// And then game validator should validate that the theme exists
