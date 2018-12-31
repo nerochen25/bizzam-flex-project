26f840f1f4fb99ad3729715bda8d927f2a23bb0a
Cjewett_work <Craig9907@gmail.com>
2018-12-31 06:42:44 -0800

    Add game routes skeleton

--- routes/api/board.js
+++ routes/api/board.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const passport = require('passport');
-const Board = require('../../models/Board')
-const validateBoardInput = require('../../validation/theme')
+const Board = require('../../models/board')
+const validateBoardInput = require('../../validation/board')
 
 
 router.post('/',
---
+++ routes/api/game.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const passport = require('passport');
+const Game = require('../../models/game')
+const validateGameInput = require('../../validation/game')
+
+
+
+
+module.exports = router;
