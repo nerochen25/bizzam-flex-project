dc57a69bda785d51cfd25b7d0436a3915920321d
Cjewett_work <Craig9907@gmail.com>
2018-12-31 05:21:47 -0800

    Add game validation and update game model

--- models/Game.js
+++ models/Game.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 
 const gameSchema = Schema({
-    boards: [{ type: Schema.Types.ObjectId, ref: 'Game' }],
+    boards: [{ type: Schema.Types.ObjectId, ref: 'Board' }],
     theme: { type: Schema.Types.ObjectId, ref: 'Theme' }, // belongs to Theme
-    winner_id: Number,
+    winner_id: {type: Number, default: 0},
     gameType: String 
 
 })
--- validation/board.js
+++ validation/board.js
@@ -3,13 +3,20 @@
 
 
 
+// Board need no validation, squares and userID are database generated
+
+// const boardSchema = Schema({
+//     userID: Number,
+//     squares: [ subSquares ]
+// })
+
+
 
 // So board should validate the user
-// userID: Number exists
+// Within the GameId this userId exists
 
 // that they are not already playing in this game (already have a board)
-// userID: Number < search the game if the userId is included already >
-
+// Within this GameId this USerID is associated with an included boardID
 
 // validate that the game exists
 
