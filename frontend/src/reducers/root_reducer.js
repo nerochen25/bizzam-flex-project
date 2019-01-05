0715d33fa56cb23dbca3668427e7fd314bcbf0e2
Cjewett_work <Craig9907@gmail.com>
2019-01-05 08:49:10 -0800

    Add games reducer to root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer';
 import boards from './boards_reducer';
+import games from './games_reducer';
 
 const RootReducer = combineReducers({
   session,
   errors,
-  boards
+  boards,
+  games
 });
 
 export default RootReducer;
