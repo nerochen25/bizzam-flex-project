0e02b2f2b6e142261a075d4ffdcdb8a4266123ee
Cjewett_work <Craig9907@gmail.com>
2019-01-05 06:46:13 -0800

    First commit of themes reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer';
 import boards from './boards_reducer';
+import themes from './themes_reducer';
 
 const RootReducer = combineReducers({
   session,
   errors,
-  boards
+  boards,
+  themes
 });
 
 export default RootReducer;
