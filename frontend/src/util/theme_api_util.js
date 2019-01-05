60e16bba62c8a93767fefe486a1a6c2f7de7b9d2
Cjewett_work <Craig9907@gmail.com>
2019-01-05 05:41:16 -0800

    Create theme actions and theme api util files

---
+++ frontend/src/util/theme_api_util.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const createTheme = data => {
+  return axios.post('/api/themes/', data)
+};
+
+export const createThemeItem = data => {
+  return axios.post('/api/themes/item', data)
+}
+
+export const createThemeItems = data => {
+  return axios.post('/api/themes/items', data)
+}
+
+
+// delete a theme
+// delete a theme item
