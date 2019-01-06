fbc70ce5a512dcc08fe29ec42b294b4c7adf1838
Cjewett_work <Craig9907@gmail.com>
2019-01-06 15:32:06 -0800

    Update multiseeding functionality acieheved pruned unecessary seed folders

---
+++ seed/team_building.js
@@ -0,0 +1,74 @@
+
+
+const rawTeamBuildingThemes = [
+    {name: "Teambuilding Demo Theme 1", description: "I have seeded random theme items"},
+    {name: "Teambuilding Demo Theme 2", description: "I have seeded random theme items"},
+    {name: "Teambuilding Demo Theme 3", description: "I have seeded random theme items"},
+    {name: "Teambuilding Demo Theme 4", description: "I have seeded random theme items"},
+    {name: "Teambuilding Demo Theme 5", description: "I have seeded random theme items"},
+    {name: "Teambuilding Demo Theme 6", description: "I have seeded random theme items"},
+    {name: "Teambuilding Demo Theme 7", description: "I have seeded random theme items"},
+    {name: "Teambuilding Demo Theme 8", description: "I have seeded random theme items"},
+    {name: "Teambuilding Demo Theme 9", description: "I have seeded random theme items"},
+    {name: "Teambuilding Demo Theme 10", description: "I have seeded random theme items"}
+];
+
+const rawTeamBuildingThemeItems = [
+
+{text:'ran in a race'},
+{text:'favorite season is fall'},
+{text:"Name starts with 'A'"},
+{text:'prefers the country rather than city'},
+{text:'knows more than 2 languages'},
+{text:'Can play an instrument'},
+{text:"Dog person"},
+{text:"Cat person"},
+{text:"prefers Pepsi to Coke"},
+{text:"Baseball fan"},
+{text:"Football fan"},
+{text:"Prefers Ninjas to Pirates"},
+{text:"Dyed his/her hair"},
+{text:"Favorite color is yellow"},
+{text:"Favorite candy is Reese's cup"},
+{text:"Has done Karaoke"},
+{text:"Has Been Arrested"},
+{text:"Has Been surfing"},
+{text:"Has Been to Asia"},
+{text:"Favorite band is the Beatles"},
+{text:"Has Gotten stitches"},
+{text:"prefers Vanilla ice cream"},
+{text:"Owns an exotic pet (not dog, cat, fish)"},
+{text:"been in a talent show"},
+{text:"bungee jumped / skydived"},
+{text:"been horseback riding"},
+{text:"Favorite fruit is grapes"},
+{text:"Read 10 books last year"},
+{text:"Has played Stratego / risk"},
+{text:"Changed a diaper"},
+{text:"Owns more than 6 pairs of shoes"},
+{text:"Has more than 3 siblings"},
+{text:"Likes Roller coasters"},
+{text:"Has Changed a tire"},
+{text:"Likes to garden"},
+{text:"Has seen Casablanca"},
+{text:"Participated in a March"},
+{text:"Plays golf"},
+{text:"Gave to the Salvation Army"},
+{text:"Owns a boat"},
+{text:"Explored a cave"},
+{text:"Used a sewing machine"},
+{text:"Grinds own coffee beans"},
+{text:"Papaya is favorite fruit"},
+{text:"Is an only child"},
+{text:"Is a middle sibling"},
+{text:"Loves sushi"}
+
+];
+
+
+
+
+module.exports = {
+    themes: rawTeamBuildingThemes,
+    themeItems: rawTeamBuildingThemeItems
+};
---
+++ seed/travel.js
@@ -0,0 +1,77 @@
+
+
+
+
+
+const rawLocationThemes = [
+    {name: "Location Demo Theme 1", description: "I have seeded random theme items"},
+    {name: "Location Demo Theme 2", description: "I have seeded random theme items"},
+    {name: "Location Demo Theme 3", description: "I have seeded random theme items"},
+    {name: "Location Demo Theme 4", description: "I have seeded random theme items"},
+    {name: "Location Demo Theme 5", description: "I have seeded random theme items"},
+    {name: "Location Demo Theme 6", description: "I have seeded random theme items"},
+    {name: "Location Demo Theme 7", description: "I have seeded random theme items"},
+    {name: "Location Demo Theme 8", description: "I have seeded random theme items"},
+    {name: "Location Demo Theme 9", description: "I have seeded random theme items"},
+    {name: "Location Demo Theme 10", description: "I have seeded random theme items"}
+];
+
+const rawLocationThemeItems = [
+    {text:'Coffeeshop'},
+    {text:'Hardware store'},
+    {text:'Best Friends house'},
+    {text:'Grocery store'},
+    {text:'Department store'},
+    {text:'Mall'},
+    {text:'Pizza Shop'},
+    {text:'Cemetary'},
+    {text:'Local Park'},
+    {text:'School'},
+    {text:'Library'},
+    {text:'LAN Party'},
+    {text:'Pub'},
+    {text:'Beach'},
+    {text:'Kitchen'},
+    {text:'Bathroom'},
+    {text:'Living Room'},
+    {text:'Backyard'},
+    {text:'Sports game'},
+    {text:'Play'},
+    {text:'Movies'},
+    {text:'Elevator'},
+    {text:'Stairs'},
+    {text:'Boat'},
+    {text:'Escalator'},
+    {text:'Bridge'},
+    {text:'Church'},
+    {text:'Castle'},
+    {text:'Hospital'},
+    {text:'Museum'},
+    {text:'Zoo'},
+    {text:'Tree'},
+    {text:'Taxi'},
+    {text:'Airport'},
+    {text:'Bed'},
+    {text:'Bus stop'},
+    {text:'Parking lot'},
+    {text:'Train station'},
+    {text:'Laundry Mat'},
+    {text:'Gym'},
+    {text:'Nail salon'},
+    {text:'Thrift Shop'},
+    {text:'Animal shelter'},
+    {text:'Ferry Boat'},
+    {text:'Tent'},
+    {text:'Hot tub'},
+    {text:'Pool'},
+    {text:'Haunted house'},
+    {text:'Butcher'},
+    {text:'Bakery'}
+
+];
+
+
+module.exports = {
+    themes: rawLocationThemes,
+    themeItems: rawLocationThemeItems
+};
